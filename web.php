566dc11dc9e3e00bb0f0486618b788980d8bbe7f
kyana1nen <34677447+kyana1nen@users.noreply.github.com>
2018-05-30 17:30:20 +0900

    Add files via upload

--- web.php
+++ web.php
@@ -24,27 +24,42 @@ Route::get('/test/{name}', function ($name) {
 });
 Route::get('/getAll',function(Request $req){
     header('Access-Control-Allow-Origin:*');
-    $post = App\Item::all();
+    $post = App\Item::where('solution',0)->orderBy("created_at","desc")->get();
     return $post;
 });
 Route::get('/lost',function(Request $req){
     header('Access-Control-Allow-Origin:*');
-    $lost = App\Item::where('status',0)->get();
+    $lost = App\Item::where('status',0)->where('solution',0)->get();
     return $lost;
 });
 Route::get('/found',function(Request $req){
     header('Access-Control-Allow-Origin:*');
-    $found = App\Item::where('status',1)->get();
+    $found = App\Item::where('status',1)->where('solution',0)->get();
     return $found;
 });
 Route::get('/search',function(Request $req){
     header('Access-Control-Allow-Origin:*');
     $search = App\Item::where('title', 'like', '%' . $req->searching . '%')
-    ->orWhere('description', 'like', '%' . $req->searching . '%')->get();
+    ->orWhere('descriptions', 'like', '%' . $req->searching . '%')->get();
     return $search;
 });
 Route::get('/advertisement',function(Request $req){
     header('Access-Control-Allow-Origin:*');
-    
-    return $req->creating2;
+    $id = App\Item::insertGetId([
+        "title"=>$req->title,
+		"descriptions"=>$req->description,
+		"contact"=>$req->contact,
+		"status"=>$req->status,
+		"solution"=>0
+    ]);
+	return $id;
+});
+Route::get('/myAdv',function(Request $req){
+    header('Access-Control-Allow-Origin:*');
+	$myAdv = App\Item::where('id',$req->itemId)->get();
+	return $myAdv;
+});
+Route::get('/removeAdv',function(Request $req){
+	header('Access-Control-Allow-Origin:*');
+	App\Item::find($req->itemId)->update(['solution' => 1]);
 });
